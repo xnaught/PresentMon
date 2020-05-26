fdbef2b2d3f060a20ffb6c0a4d6e504e51a5a3c4
Jefferson Montgomery <JeffersonMontgomery-Intel@users.noreply.github.com>
2020-05-25 22:12:07 -0700

    Don't run PresentMonTestEtls.exe from script after downloading it.
    Windows security thinks it's a virus in that case.  Either have user run
    it themselves, or use 7z to decompress if it's in the path.

--- Tools/download_test_etls.cmd
+++ Tools/download_test_etls.cmd
@@ -3,7 +3,9 @@ setlocal
 set src="https://github.com/GameTechDev/PresentMon/releases/download/PresentMonTestEtls/PresentMonTestEtls.exe"
 set dst="%~dp0..\Tests\Gold\PresentMonTestEtls.exe"
 set ba=
+set sz=
 for %%a in (bitsadmin.exe) do set ba=%%~$PATH:a
+for %%a in (7z.exe)        do set sz=%%~$PATH:a
 if "%ba%"=="" goto manual
 if not exist "%~dp0..\Tests\Gold\." goto manual
 
@@ -14,7 +16,18 @@ if not exist %dst% goto manual
 
 echo Uncompressing PresentMonTestEtls.exe
 pushd "%~dp0..\Tests\Gold"
-PresentMonTestEtls.exe
+rem BEGIN WORKAROUND: Windows security seems to think it's a virus if I try to
+rem run it right away, so get user to run it explicitly or use 7z if it is
+rem available.
+::PresentMonTestEtls.exe
+if "%sz%"=="" (
+    echo Run PresentMonTestEtls.exe in the Tests\Gold directory to uncompress the test ETLs
+    explorer .
+    pause
+) else (
+    "%sz%" x PresentMonTestEtls.exe > NUL
+)
+:: END WORKAROUND
 popd
 exit /b 0
 
