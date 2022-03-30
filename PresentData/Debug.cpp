xnaught/PresentMon

commit 889af66a325b728c2160689044a45b44dd47bd7b

Add enums to etw_list output
Also:
- remove non-c++ output path
- changes command arguments to default to show all (--show=... -> --no_...)

PresentData/Debug.cpp
@@ -97,42 +97,45 @@ void PrintPresentResult(PresentResult value)
 }
 void PrintPresentHistoryModel(uint32_t model)
 {
+    using namespace Microsoft_Windows_DxgKrnl;
     switch (model) {
-    case D3DKMT_PM_UNINITIALIZED:          printf("UNINITIALIZED");          break;
-    case D3DKMT_PM_REDIRECTED_GDI:         printf("REDIRECTED_GDI");         break;
-    case D3DKMT_PM_REDIRECTED_FLIP:        printf("REDIRECTED_FLIP");        break;
-    case D3DKMT_PM_REDIRECTED_BLT:         printf("REDIRECTED_BLT");         break;
-    case D3DKMT_PM_REDIRECTED_VISTABLT:    printf("REDIRECTED_VISTABLT");    break;
-    case D3DKMT_PM_SCREENCAPTUREFENCE:     printf("SCREENCAPTUREFENCE");     break;
-    case D3DKMT_PM_REDIRECTED_GDI_SYSMEM:  printf("REDIRECTED_GDI_SYSMEM");  break;
-    case D3DKMT_PM_REDIRECTED_COMPOSITION: printf("REDIRECTED_COMPOSITION"); break;
-    default:                               printf("Unknown (%u)", model); assert(false); break;
+    case PresentModel::D3DKMT_PM_UNINITIALIZED:          printf("UNINITIALIZED");          break;
+    case PresentModel::D3DKMT_PM_REDIRECTED_GDI:         printf("REDIRECTED_GDI");         break;
+    case PresentModel::D3DKMT_PM_REDIRECTED_FLIP:        printf("REDIRECTED_FLIP");        break;
+    case PresentModel::D3DKMT_PM_REDIRECTED_BLT:         printf("REDIRECTED_BLT");         break;
+    case PresentModel::D3DKMT_PM_REDIRECTED_VISTABLT:    printf("REDIRECTED_VISTABLT");    break;
+    case PresentModel::D3DKMT_PM_SCREENCAPTUREFENCE:     printf("SCREENCAPTUREFENCE");     break;
+    case PresentModel::D3DKMT_PM_REDIRECTED_GDI_SYSMEM:  printf("REDIRECTED_GDI_SYSMEM");  break;
+    case PresentModel::D3DKMT_PM_REDIRECTED_COMPOSITION: printf("REDIRECTED_COMPOSITION"); break;
+    default:                                             printf("Unknown (%u)", model); assert(false); break;
     }
 }
 void PrintTokenState(uint32_t state)
 {
+    using namespace Microsoft_Windows_Win32k;
     switch (state) {
-    case Microsoft_Windows_Win32k::TokenState::Completed: printf("Completed"); break;
-    case Microsoft_Windows_Win32k::TokenState::InFrame:   printf("InFrame");   break;
-    case Microsoft_Windows_Win32k::TokenState::Confirmed: printf("Confirmed"); break;
-    case Microsoft_Windows_Win32k::TokenState::Retired:   printf("Retired");   break;
-    case Microsoft_Windows_Win32k::TokenState::Discarded: printf("Discarded"); break;
-    default:                                              printf("Unknown (%u)", state); assert(false); break;
+    case TokenState::Completed: printf("Completed"); break;
+    case TokenState::InFrame:   printf("InFrame");   break;
+    case TokenState::Confirmed: printf("Confirmed"); break;
+    case TokenState::Retired:   printf("Retired");   break;
+    case TokenState::Discarded: printf("Discarded"); break;
+    default:                    printf("Unknown (%u)", state); assert(false); break;
     }
 }
 void PrintQueuePacketType(uint32_t type)
 {
+    using namespace Microsoft_Windows_DxgKrnl;
     switch (type) {
-    case DXGKETW_RENDER_COMMAND_BUFFER:   printf("RENDER"); break;
-    case DXGKETW_DEFERRED_COMMAND_BUFFER: printf("DEFERRED"); break;
-    case DXGKETW_SYSTEM_COMMAND_BUFFER:   printf("SYSTEM"); break;
-    case DXGKETW_MMIOFLIP_COMMAND_BUFFER: printf("MMIOFLIP"); break;
-    case DXGKETW_WAIT_COMMAND_BUFFER:     printf("WAIT"); break;
-    case DXGKETW_SIGNAL_COMMAND_BUFFER:   printf("SIGNAL"); break;
-    case DXGKETW_DEVICE_COMMAND_BUFFER:   printf("DEVICE"); break;
-    case DXGKETW_SOFTWARE_COMMAND_BUFFER: printf("SOFTWARE"); break;
-    case DXGKETW_PAGING_COMMAND_BUFFER:   printf("PAGING"); break;
-    default:                              printf("Unknown (%u)", type); assert(false); break;
+    case QueuePacketType::DXGKETW_RENDER_COMMAND_BUFFER:   printf("RENDER"); break;
+    case QueuePacketType::DXGKETW_DEFERRED_COMMAND_BUFFER: printf("DEFERRED"); break;
+    case QueuePacketType::DXGKETW_SYSTEM_COMMAND_BUFFER:   printf("SYSTEM"); break;
+    case QueuePacketType::DXGKETW_MMIOFLIP_COMMAND_BUFFER: printf("MMIOFLIP"); break;
+    case QueuePacketType::DXGKETW_WAIT_COMMAND_BUFFER:     printf("WAIT"); break;
+    case QueuePacketType::DXGKETW_SIGNAL_COMMAND_BUFFER:   printf("SIGNAL"); break;
+    case QueuePacketType::DXGKETW_DEVICE_COMMAND_BUFFER:   printf("DEVICE"); break;
+    case QueuePacketType::DXGKETW_SOFTWARE_COMMAND_BUFFER: printf("SOFTWARE"); break;
+    case QueuePacketType::DXGKETW_PAGING_COMMAND_BUFFER:   printf("PAGING"); break;
+    default:                                               printf("Unknown (%u)", type); assert(false); break;
     }
 }
 void PrintPresentFlags(uint32_t flags)

PresentData/ETW/Microsoft_Windows_D3D9.h
@@ -1,8 +1,9 @@
-// This file originally generated by etw_list
-//     version:    development branch 1f6ac03eede3e9ab6a6bae5c6de68ad6dd75312d
-//     parameters: --show=all --output=c++ --event=Present::Start --event=Present::Stop --provider=Microsoft-Windows-D3D9
 // Copyright (C) 2020-2021 Intel Corporation
 // SPDX-License-Identifier: MIT
+//
+// This file originally generated by etw_list
+//     version:    development branch e5985e637875db6cb6f90e8c92d0857b6fb95324
+//     parameters: --no_event_structs --event=Present::Start --event=Present::Stop --provider=Microsoft-Windows-D3D9
 #pragma once
 
 namespace Microsoft_Windows_D3D9 {
@@ -39,23 +40,13 @@ EVENT_DESCRIPTOR_DECL(Present_Stop , 0x0002, 0x00, 0x10, 0x00, 0x02, 0x0001, 0x8
 
 #undef EVENT_DESCRIPTOR_DECL
 
-#pragma warning(push)
-#pragma warning(disable: 4200) // nonstandard extension used: zero-sized array in struct
-
-#pragma pack(push)
-#pragma pack(1)
-
-template<typename PointerT>
-struct Present_Start_Struct {
-    PointerT    pSwapchain;
-    uint32_t    Flags;
+enum class D3D9PresentFlags : uint32_t {
+    D3DPRESENT_DONOTWAIT = 1,
+    D3DPRESENT_LINEAR_CONTENT = 2,
+    D3DPRESENT_DONOTFLIP = 4,
+    D3DPRESENT_FLIPRESTART = 8,
+    D3DPRESENT_VIDEO_RESTRICT_TO_MONITOR = 16,
+    D3DPRESENT_FORCEIMMEDIATE = 256,
 };
-
-struct Present_Stop_Struct {
-    uint32_t    Result;
-};
-
-#pragma pack(pop)
-#pragma warning(pop)
 
 }

PresentData/ETW/Microsoft_Windows_DXGI.h
@@ -1,8 +1,9 @@
-// This file originally generated by etw_list
-//     version:    development branch 1f6ac03eede3e9ab6a6bae5c6de68ad6dd75312d
-//     parameters: --show=all --output=c++ --event=Present::Start --event=Present::Stop --event=PresentMultiplaneOverlay::Start --event=PresentMultiplaneOverlay::Stop --provider=Microsoft-Windows-DXGI
 // Copyright (C) 2020-2021 Intel Corporation
 // SPDX-License-Identifier: MIT
+//
+// This file originally generated by etw_list
+//     version:    development branch e5985e637875db6cb6f90e8c92d0857b6fb95324
+//     parameters: --no_event_structs --event=Present::Start --event=Present::Stop --event=PresentMultiplaneOverlay::Start --event=PresentMultiplaneOverlay::Stop --provider=Microsoft-Windows-DXGI
 #pragma once
 
 namespace Microsoft_Windows_DXGI {
@@ -38,46 +39,21 @@ enum class Channel : uint8_t {
     static Keyword  const Keyword = (Keyword) keyword_; \
 };
 
-EVENT_DESCRIPTOR_DECL(Present_Start                 , 0x002a, 0x00, 0x10, 0x00, 0x01, 0x0009, 0x8000000000000002)
-EVENT_DESCRIPTOR_DECL(Present_Stop                  , 0x002b, 0x00, 0x10, 0x00, 0x02, 0x0009, 0x8000000000000002)
 EVENT_DESCRIPTOR_DECL(PresentMultiplaneOverlay_Start, 0x0037, 0x00, 0x10, 0x00, 0x01, 0x000e, 0x8000000000000002)
 EVENT_DESCRIPTOR_DECL(PresentMultiplaneOverlay_Stop , 0x0038, 0x00, 0x10, 0x00, 0x02, 0x000e, 0x8000000000000002)
+EVENT_DESCRIPTOR_DECL(Present_Start                 , 0x002a, 0x00, 0x10, 0x00, 0x01, 0x0009, 0x8000000000000002)
+EVENT_DESCRIPTOR_DECL(Present_Stop                  , 0x002b, 0x00, 0x10, 0x00, 0x02, 0x0009, 0x8000000000000002)
 
 #undef EVENT_DESCRIPTOR_DECL
 
-#pragma warning(push)
-#pragma warning(disable: 4200) // nonstandard extension used: zero-sized array in struct
-
-#pragma pack(push)
-#pragma pack(1)
-
-template<typename PointerT>
-struct Present_Start_Struct {
-    PointerT    pIDXGISwapChain;
-    uint32_t    Flags;
-    uint32_t    SyncInterval;
-    uint32_t    DirtyRects;
-    uint32_t    ScrollRects;
+enum class DXGIPresentFlags : uint32_t {
+    DXGI_PRESENT_TEST = 1,
+    DXGI_PRESENT_DO_NOT_SEQUENCE = 2,
+    DXGI_PRESENT_RESTART = 4,
+    DXGI_PRESENT_DO_NOT_WAIT = 8,
+    DXGI_PRESENT_STEREO_PREFER_RIGHT = 16,
+    DXGI_PRESENT_STEREO_TEMPORARY_MONO = 32,
+    DXGI_PRESENT_RESTRICT_TO_OUTPUT = 64,
 };
-
-struct Present_Stop_Struct {
-    uint32_t    Result;
-};
-
-template<typename PointerT>
-struct PresentMultiplaneOverlay_Start_Struct {
-    PointerT    pIDXGISwapChain;
-    uint32_t    Flags;
-    uint32_t    SyncInterval;
-    uint32_t    NumPlanes;
-    uint32_t    LayerMask;
-};
-
-struct PresentMultiplaneOverlay_Stop_Struct {
-    uint32_t    Result;
-};
-
-#pragma pack(pop)
-#pragma warning(pop)
 
 }

PresentData/ETW/Microsoft_Windows_Dwm_Core.h
@@ -1,8 +1,9 @@
-// This file originally generated by etw_list
-//     version:    development branch 1f6ac03eede3e9ab6a6bae5c6de68ad6dd75312d
-//     parameters: --show=all --output=c++ --event=MILEVENT_MEDIA_UCE_PROCESSPRESENTHISTORY_GetPresentHistory::Info --event=SCHEDULE_PRESENT::Start --event=SCHEDULE_SURFACEUPDATE::Info --provider=Microsoft-Windows-Dwm-Core
 // Copyright (C) 2020-2021 Intel Corporation
 // SPDX-License-Identifier: MIT
+//
+// This file originally generated by etw_list
+//     version:    development branch e5985e637875db6cb6f90e8c92d0857b6fb95324
+//     parameters: --no_event_structs --event=MILEVENT_MEDIA_UCE_PROCESSPRESENTHISTORY_GetPresentHistory::Info --event=SCHEDULE_PRESENT::Start --event=SCHEDULE_SURFACEUPDATE::Info --provider=Microsoft-Windows-Dwm-Core
 #pragma once
 
 namespace Microsoft_Windows_Dwm_Core {
@@ -62,56 +63,10 @@ EVENT_DESCRIPTOR_DECL(SCHEDULE_PRESENT_Start                                    
 EVENT_DESCRIPTOR_DECL(SCHEDULE_SURFACEUPDATE_Info                                    , 0x00c4, 0x00, 0x10, 0x04, 0x00, 0x007f, 0x8000000000000001)
 
 // These events added manually:
-struct FlipChain_Pending  { static uint16_t const Id = 0x0045; };
-struct FlipChain_Complete { static uint16_t const Id = 0x0046; };
-struct FlipChain_Dirty    { static uint16_t const Id = 0x0065; };
+EVENT_DESCRIPTOR_DECL(FlipChain_Complete                                             , 0x0046, 0x00, 0x00, 0x00, 0x00, 0x0000, 0x0000000000000000)
+EVENT_DESCRIPTOR_DECL(FlipChain_Dirty                                                , 0x0065, 0x00, 0x00, 0x00, 0x00, 0x0000, 0x0000000000000000)
+EVENT_DESCRIPTOR_DECL(FlipChain_Pending                                              , 0x0045, 0x00, 0x00, 0x00, 0x00, 0x0000, 0x0000000000000000)
 
 #undef EVENT_DESCRIPTOR_DECL
-
-#pragma warning(push)
-#pragma warning(disable: 4200) // nonstandard extension used: zero-sized array in struct
-
-#pragma pack(push)
-#pragma pack(1)
-
-struct MILEVENT_MEDIA_UCE_PROCESSPRESENTHISTORY_GetPresentHistory_Info_Struct {
-    uint32_t    hr;
-    uint32_t    cTokenCount;
-};
-
-struct SCHEDULE_PRESENT_Start_Struct {
-    uint64_t    tCurrent;
-    uint64_t    tPresent;
-    uint64_t    cRefreshCurrent;
-    uint64_t    cRefreshPresent;
-    uint32_t    fForce;
-};
-
-struct SCHEDULE_SURFACEUPDATE_Info_MemberStruct_1_Struct {
-    uint32_t    lowpart;
-    uint32_t    highpart;
-};
-
-struct SCHEDULE_SURFACEUPDATE_Info_Struct {
-    struct SCHEDULE_SURFACEUPDATE_Info_MemberStruct_1_Struct luidSurface[1];
-    uint64_t    bindId;
-    uint64_t    PresentCount;
-    uint64_t    fenceValue;
-    uint32_t    bDirectFlip;
-    uint32_t    DXGI_ALPHA_MODE;
-    uint64_t    hmonAssociation;
-    uint32_t    bStereoPreferRight;
-    uint32_t    bTemporaryMono;
-    uint32_t    bSwapPool;
-    uint32_t    BufferContentType;
-    uint32_t    bIndependentFlip;
-    uint32_t    uPesentDuration;
-    uint32_t    BufferRealizationType;
-    uint32_t    uRealizationIndex;
-    uint64_t    hDxSurface;
-};
-
-#pragma pack(pop)
-#pragma warning(pop)
 
 }

commit bc25e0ce6ea607249fc193926199a6bd03a8fe21

Fix DEBUG_VERBOSE build broken in 889af66a3

PresentData/Debug.cpp
@@ -229,7 +229,7 @@ void FlushModifiedPresent()
 
 }
 
-void DebugInitialize(LARGE_INTEGER* firstTimestamp, LARGE_INTEGER timestampFrequency)
+void DebugInitialize(LARGE_INTEGER* firstTimestamp, LARGE_INTEGER const& timestampFrequency)
 {
     gDebugDone = false;
     gFirstTimestamp = firstTimestamp;

PresentData/Debug.hpp
@@ -15,9 +15,10 @@
 struct PresentEvent; // Can't include PresentMonTraceConsumer.hpp because it includes Debug.hpp (before defining PresentEvent)
 struct EventMetadata;
 struct _EVENT_RECORD;
+union _LARGE_INTEGER;
 
 // Initialize debug system
-void DebugInitialize(LARGE_INTEGER* firstTimestamp, LARGE_INTEGER timestampFrequency);
+void DebugInitialize(_LARGE_INTEGER* firstTimestamp, _LARGE_INTEGER const& timestampFrequency);
 
 // Check if debug is complete
 bool DebugDone();