422f05f7585ee64a91b7b3d1cbc208f616ed552f
Chili <planetchili@users.noreply.github.com>
2023-11-15 16:16:37 +0900

    moving datastruct to header to expose to tests

--- IntelPresentMon/Interprocess/source/ExperimentalInterprocess.cpp
+++ IntelPresentMon/Interprocess/source/ExperimentalInterprocess.cpp
@@ -1,14 +1,8 @@
 #include "ExperimentalInterprocess.h"
-#include <boost/interprocess/managed_windows_shared_memory.hpp>
-#include <boost/interprocess/containers/string.hpp>
-#include <boost/interprocess/allocators/allocator.hpp>
-#include <boost/interprocess/smart_ptr/unique_ptr.hpp>
 #include <memory>
 
 namespace pmon::ipc::experimental
 {
-	namespace bip = boost::interprocess;
-
 	using ShmSegment = bip::managed_windows_shared_memory;
 	using ShmSegmentManager = ShmSegment::segment_manager;
 	template<typename T>
@@ -19,64 +13,6 @@ namespace pmon::ipc::experimental
 	template<typename T>
 	using Uptr = bip::unique_ptr<T, UptrDeleter<T>>;
 
-	using bip::ipcdetail::to_raw_pointer;
-
-	// <A> is allocator already adapted to handling the type T its meant to handle
-	template<class A>
-	struct AllocatorDeleter
-	{
-		using T = typename A::value_type;
-		using pointer = std::allocator_traits<A>::pointer;
-		AllocatorDeleter(A allocator_) : allocator{ std::move(allocator_) } {}
-		void operator()(pointer ptr) { allocator.deallocate(to_raw_pointer(ptr), 1u); }
-	private:
-		A allocator;
-	};
-
-	template<template <class> typename T, class A>
-	using UptrT = bip::unique_ptr<T<A>, AllocatorDeleter<typename T<A>::Allocator>>;
-
-	template<template <class> typename T, class A, typename...P>
-	auto MakeUnique(A allocator_in, P&&...args)
-	{
-		// allocator type for creating the object to be managed by the uptr
-		using Allocator = typename T<A>::Allocator;
-		// deleter type for the uptr (derived from the allocator of the managed object)
-		using Deleter = typename UptrT<T, A>::deleter_type;
-		// create allocator for managed object
-		Allocator allocator(std::move(allocator_in));
-		// allocate memory for managed object (uninitialized)
-		auto ptr = allocator.allocate(1);
-		// construct object in allocated memory
-		std::allocator_traits<Allocator>::construct(allocator, to_raw_pointer(ptr), std::forward<P>(args)...);
-		// construct uptr and deleter
-		return UptrT<T, A>(to_raw_pointer(ptr), Deleter(allocator));
-	}
-
-	template<class A>
-	class Branch
-	{
-	public:
-		// allocator type for this instance, based on the allocator type used to template
-		using Allocator = std::allocator_traits<A>::template rebind_alloc<Branch>;
-		Branch(int x_) : x{ x_ } {}
-		int Get() const { return x; }
-	private:
-		int x;
-	};
-
-	template<class A>
-	class Root
-	{
-	public:
-		Root(int x, A allocator)
-			:
-			pBranch{ MakeUnique<Branch, A>(allocator, x) }
-		{}
-		int Get() const { return pBranch->Get(); }
-	private:
-		UptrT<Branch, A> pBranch;
-	};
 
 	class Server : public IServer
 	{
