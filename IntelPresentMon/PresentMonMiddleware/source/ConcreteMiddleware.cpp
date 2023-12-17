4b666305aa5e2e069ab1e3df8a7d7d2da4951fce
Chili <planetchili@users.noreply.github.com>
2023-12-18 08:40:57 +0900

    metrics filled and sorted

--- IntelPresentMon/PresentMonMiddleware/source/ConcreteMiddleware.cpp
+++ IntelPresentMon/PresentMonMiddleware/source/ConcreteMiddleware.cpp
@@ -375,7 +375,7 @@ namespace pmon::mid
             case PM_METRIC_PRESENT_MODE:
             case PM_METRIC_PRESENT_RUNTIME:
             case PM_METRIC_PRESENT_QPC:
-            case PM_METRIC_PRESENT_SYNC_INTERVAL:
+            case PM_METRIC_SYNC_INTERVAL:
             case PM_METRIC_DROPPED_FRAMES:
             case PM_METRIC_ALLOWS_TEARING:
             case PM_METRIC_NUM_PRESENTS:
@@ -889,7 +889,7 @@ namespace pmon::mid
             auto& output = reinterpret_cast<uint64_t&>(pBlob[element.dataOffset]);
             output = swapChain.frameQpc;
         }
-        else if (element.metric == PM_METRIC_PRESENT_SYNC_INTERVAL)
+        else if (element.metric == PM_METRIC_SYNC_INTERVAL)
         {
             auto& output = reinterpret_cast<uint32_t&>(pBlob[element.dataOffset]);
             output = swapChain.sync_interval;
@@ -1438,7 +1438,7 @@ namespace pmon::mid
                 case PM_METRIC_PRESENT_RUNTIME:
                 case PM_METRIC_PRESENT_QPC:
                 case PM_METRIC_PROCESS_NAME:
-                case PM_METRIC_PRESENT_SYNC_INTERVAL:
+                case PM_METRIC_SYNC_INTERVAL:
                 case PM_METRIC_DROPPED_FRAMES:
                 case PM_METRIC_ALLOWS_TEARING:
                 case PM_METRIC_NUM_PRESENTS:
