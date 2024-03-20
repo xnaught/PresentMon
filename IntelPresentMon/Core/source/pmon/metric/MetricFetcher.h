38c4d9d8a7155a90cbd9f4808fdd94421eb8428e
Taylor, Raymond <raymond.taylor@intel.com>
2024-03-20 15:55:50 +0900

    fix #1981991

--- IntelPresentMon/Core/source/pmon/metric/MetricFetcher.h
+++ IntelPresentMon/Core/source/pmon/metric/MetricFetcher.h
@@ -9,8 +9,14 @@ namespace p2c::pmon::met
     class MetricFetcher
     {
     public:
+        MetricFetcher() = default;
         virtual ~MetricFetcher();
         virtual std::wstring ReadStringValue();
         virtual std::optional<float> ReadValue() = 0;
+
+        MetricFetcher(const MetricFetcher&) = delete;
+        MetricFetcher & operator=(const MetricFetcher&) = delete;
+        MetricFetcher(MetricFetcher&&) = delete;
+        MetricFetcher & operator=(MetricFetcher&&) = delete;
     };
 }
