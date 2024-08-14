79588bd157af185eeb45915ec50c301975d2d4b0
Chili <planetchili@users.noreply.github.com>
2024-08-14 09:45:37 +0900

    automatically generating AllActions.h => plug and play actions complete

--- IntelPresentMon/PresentMonService/AllActions.h
+++ IntelPresentMon/PresentMonService/AllActions.h
@@ -1,8 +1,8 @@
-#pragma once
-#include "acts/OpenSession.h"
-#include "acts/GetStaticCpuMetrics.h"
-#include "acts/StartTracking.h"
-#include "acts/EnumerateAdapters.h"
-#include "acts/SetTelemetryPeriod.h"
-#include "acts/SelectAdapter.h"
-#include "acts/StopTracking.h"
+#pragma once 
+#include "acts/EnumerateAdapters.h" 
+#include "acts/GetStaticCpuMetrics.h" 
+#include "acts/OpenSession.h" 
+#include "acts/SelectAdapter.h" 
+#include "acts/SetTelemetryPeriod.h" 
+#include "acts/StartTracking.h" 
+#include "acts/StopTracking.h" 
