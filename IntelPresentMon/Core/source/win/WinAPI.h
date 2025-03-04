ed200b77620902d344fd7e12909143cda8cb8157
Chili <planetchili@users.noreply.github.com>
2025-03-04 12:43:21 +0900

    use common winapi include in ipm.core

--- IntelPresentMon/Core/source/win/WinAPI.h
+++ IntelPresentMon/Core/source/win/WinAPI.h
@@ -1,12 +1,7 @@
 // Copyright (C) 2022 Intel Corporation
 // SPDX-License-Identifier: MIT
 #pragma once
-#define _WIN32_WINNT 0x0602
-#include <sdkddkver.h>
-#define WIN32_LEAN_AND_MEAN
-#define NOMINMAX
-#define STRICT
-#include <windows.h>
+#include <CommonUtilities/win/WinAPI.h>
 // remove annoying A/W macros as necessary
 #undef FormatMessage
 #include <Core/source/gfx/base/Geometry.h>
