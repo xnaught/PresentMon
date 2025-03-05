aaf22f0ccff5f32ab9672c897f6415b4d89bce3e
Chili <planetchili@users.noreply.github.com>
2025-03-06 06:06:53 +0900

    error when middleware doesn't match service

--- IntelPresentMon/Versioning/BuildId.cpp
+++ IntelPresentMon/Versioning/BuildId.cpp
@@ -1,6 +1,14 @@
 #include "BuildId.h"
 #include "generated/build_id.h"
 
+#ifndef PM_BUILD_CONFIG_
+#ifdef NDEBUG
+#define PM_BUILD_CONFIG_ "rel"
+#else
+#define PM_BUILD_CONFIG_ "dbg"
+#endif
+#endif
+
 namespace pmon::bid
 {
 	const char* BuildIdShortHash() noexcept
@@ -18,6 +26,10 @@ namespace pmon::bid
 	const char* BuildIdUid() noexcept
 	{
 		return PM_BID_UID;
+	}
+	const char* BuildIdConfig() noexcept
+	{
+		return PM_BUILD_CONFIG_;
 	}
 	bool BuildIdDirtyFlag() noexcept
 	{
