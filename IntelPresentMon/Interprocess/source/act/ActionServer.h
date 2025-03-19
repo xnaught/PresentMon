f49075a9a9c7bca3caa15b9c31d3a8c562739508
Chili <planetchili@users.noreply.github.com>
2025-03-19 15:57:59 +0900

    move action server impl into ipc project

---
+++ IntelPresentMon/Interprocess/source/act/ActionServer.h
@@ -0,0 +1,123 @@
+// Copyright (C) 2022 Intel Corporation
+// SPDX-License-Identifier: MIT
+#include "../../../CommonUtilities/pipe/Pipe.h"
+#include "../../../CommonUtilities/str/String.h"
+#include "AsyncActionManager.h"
+#include <thread>
+#include <sddl.h>
+
+
+namespace pmon::ipc::act
+{
+    using namespace std::literals;
+    using namespace pmon;
+    using namespace util;
+    using namespace ipc;
+    namespace as = boost::asio;
+
+    template<class ExecCtx>
+    class ActionServerImpl_
+    {
+    public:
+        ActionServerImpl_(ExecCtx context, std::string pipeName,
+            uint32_t reservedPipeInstanceCount, std::string securityString)
+            :
+            reservedPipeInstanceCount_{ reservedPipeInstanceCount },
+            pipeName_{ std::move(pipeName) },
+            security_{ std::move(securityString) },
+            // stopEvent_{ ioctx_, win::Handle::CreateCloned(pSvc->GetServiceStopHandle()).Release() },
+            actionManager_{ std::move(context) }
+        {
+            assert(reservedPipeInstanceCount_ > 0);
+        }
+        void Run_()
+        {
+            try {
+                // there are issues with calling .stop() and subsequently destroying ioctx
+                // for now we will detach the thread and allow it to run independently until
+                // process exit
+                // 
+                //// coroutine to exit thread on stop signal
+                //as::co_spawn(ioctx_, [this]() -> as::awaitable<void> {
+                //    co_await stopEvent_.async_wait(as::use_awaitable);
+                //    pmlog_dbg("ActionServer received stop signal");
+                //    ioctx_.stop();
+                //    }, as::detached);
+
+                // maintain N available pipe instances at all times
+                for (uint32_t i = 0; i < reservedPipeInstanceCount_; i++) {
+                    as::co_spawn(ioctx_, AcceptConnection_(), as::detached);
+                }
+                // run the io context event handler until signalled to exit
+                ioctx_.run();
+                pmlog_info("ActionServer exiting");
+            }
+            catch (...) {
+                pmlog_error(ReportException());
+                // if the action server crashes for any reason, the service should restart at this point
+                log::GetDefaultChannel()->Flush();
+                std::terminate();
+            }
+        }
+        static std::thread LaunchThread(ExecCtx context, std::string pipeName,
+            uint32_t maxPipeInstanceCount, std::string securityString)
+        {
+            return std::thread([](auto&& context, std::string pipeName,
+                uint32_t maxPipeInstanceCount, std::string securityString) {
+                ActionServerImpl_ impl(std::move(context), std::move(pipeName),
+                    maxPipeInstanceCount, std::move(securityString));
+                impl.Run_();
+            }, std::move(context), std::move(pipeName), maxPipeInstanceCount, std::move(securityString));
+        }
+
+        ActionServerImpl_(const ActionServerImpl_&) = delete;
+        ActionServerImpl_& operator=(const ActionServerImpl_&) = delete;
+        ActionServerImpl_(ActionServerImpl_&&) = delete;
+        ActionServerImpl_& operator=(ActionServerImpl_&&) = delete;
+        ~ActionServerImpl_() = default;
+    private:
+        // functions
+        as::awaitable<void> AcceptConnection_()
+        {
+            auto& sessions = actionManager_.ctx_.sessions;
+            std::optional<uint32_t> sessionId;
+            try {
+                // create pipe instance object
+                std::shared_ptr pPipe = pipe::DuplexPipe::MakeAsPtr(pipeName_, ioctx_, security_);
+                // wait for a client to connect
+                co_await pPipe->Accept();
+                // insert a session context object for this connection, will be initialized properly upon OpenSession action
+                sessionId = pPipe->GetId();
+                sessions[*sessionId].pPipe = pPipe;
+                pmlog_info(std::format("Action pipe connected id:{}", *sessionId));
+                // fork this acceptor coroutine
+                as::co_spawn(ioctx_, AcceptConnection_(), as::detached);
+                // run the action handler until client session is terminated
+                while (true) {
+                    co_await actionManager_.SyncHandleRequest(*pPipe);
+                }
+            }
+            catch (...) {
+                pmlog_dbg(util::ReportException());
+                if (sessionId) {
+                    uint32_t clientPid = 0;
+                    if (auto i = sessions.find(*sessionId); i != sessions.end()) {
+                        clientPid = i->second.clientPid;
+                    }
+                    actionManager_.ctx_.DisposeSession(*sessionId);
+                    pmlog_info(std::format("Action pipe disconnected, session closed id:{} pid:{}", *sessionId, clientPid));
+                    co_return;
+                }
+                pmlog_info("Sessionless pipe disconnected");
+            }
+        }
+        // data
+        uint32_t reservedPipeInstanceCount_;
+        uint32_t maxPipeInstanceCount_;
+        std::string pipeName_;
+        std::string security_;
+        as::io_context ioctx_;
+        // as::windows::object_handle stopEvent_;
+        act::AsyncActionManager<ExecCtx> actionManager_;
+    };
+}
--- IntelPresentMon/PresentMonService/ActionServer.cpp
+++ IntelPresentMon/PresentMonService/ActionServer.cpp
@@ -2,124 +2,26 @@
 // SPDX-License-Identifier: MIT
 #include "../CommonUtilities/pipe/Pipe.h"
 #include "../CommonUtilities/str/String.h"
-#include "../Interprocess/source/act/AsyncActionManager.h"
-#include "CliOptions.h"
+#include "../Interprocess/source/act/ActionServer.h"
 #include "ActionServer.h"
 #include "GlobalIdentifiers.h"
 #include "ServiceExecutionContext.h"
-#include <thread>
-#include <sddl.h>
 
 
 namespace pmon::svc
 {
-    using namespace std::literals;
     using namespace pmon;
     using namespace util;
     using namespace ipc;
-    namespace as = boost::asio;
-
-    class ActionServerImpl_
-    {
-    public:
-        ActionServerImpl_(Service* pSvc, PresentMon* pPmon, std::optional<std::string> pipeName)
-            :
-            pipeName_{ pipeName.value_or(gid::defaultControlPipeName) },
-            elevatedSecurity_{ !pipeName },
-            stopEvent_{ ioctx_, win::Handle::CreateCloned(pSvc->GetServiceStopHandle()).Release() }
-        {
-            actionManager_.ctx_.pSvc = pSvc;
-            actionManager_.ctx_.pPmon = pPmon;
-        }
-        void Run_()
-        {
-            try {
-                // there are issues with calling .stop() and subsequently destroying ioctx
-                // for now we will detach the thread and allow it to run independently until
-                // process exit
-                // 
-                //// coroutine to exit thread on stop signal
-                //as::co_spawn(ioctx_, [this]() -> as::awaitable<void> {
-                //    co_await stopEvent_.async_wait(as::use_awaitable);
-                //    pmlog_dbg("ActionServer received stop signal");
-                //    ioctx_.stop();
-                //    }, as::detached);
-                
-                // maintain 2 available pipe instances at all times
-                for (int i = 0; i < 2; i++) {
-                    as::co_spawn(ioctx_, AcceptConnection_(), as::detached);
-                }
-                // run the io context event handler until signalled to exit
-                ioctx_.run();
-                pmlog_info("ActionServer exiting");
-            }
-            catch (...) {
-                pmlog_error(ReportException());
-                // if the action server crashes for any reason, the service should restart at this point
-                log::GetDefaultChannel()->Flush();
-                std::terminate();
-            }
-        }
-
-        ActionServerImpl_(const ActionServerImpl_&) = delete;
-        ActionServerImpl_& operator=(const ActionServerImpl_&) = delete;
-        ActionServerImpl_(ActionServerImpl_&&) = delete;
-        ActionServerImpl_& operator=(ActionServerImpl_&&) = delete;
-        ~ActionServerImpl_() = default;
-    private:
-        // functions
-        as::awaitable<void> AcceptConnection_()
-        {
-            auto& sessions = actionManager_.ctx_.sessions;
-            std::optional<uint32_t> sessionId;
-            try {
-                // create pipe instance object
-                std::shared_ptr pPipe = pipe::DuplexPipe::MakeAsPtr(pipeName_, ioctx_, GetSecurityString_());
-                // wait for a client to connect
-                co_await pPipe->Accept();
-                // insert a session context object for this connection, will be initialized properly upon OpenSession action
-                sessionId = pPipe->GetId();
-                sessions[*sessionId].pPipe = pPipe;
-                pmlog_info(std::format("Action pipe connected id:{}", *sessionId));
-                // fork this acceptor coroutine
-                as::co_spawn(ioctx_, AcceptConnection_(), as::detached);
-                // run the action handler until client session is terminated
-                while (true) {
-                    co_await actionManager_.SyncHandleRequest(*pPipe);
-                }
-            }
-            catch (...) {
-                pmlog_dbg(util::ReportException());
-                if (sessionId) {
-                    uint32_t clientPid = 0;
-                    if (auto i = sessions.find(*sessionId); i != sessions.end()) {
-                        clientPid = i->second.clientPid;
-                    }
-                    actionManager_.ctx_.DisposeSession(*sessionId);
-                    pmlog_info(std::format("Action pipe disconnected, session closed id:{} pid:{}", *sessionId, clientPid));
-                    co_return;
-                }
-                pmlog_info("Sessionless pipe disconnected");
-            }
-        }
-        std::string GetSecurityString_() const
-        {
-            return pipe::DuplexPipe::GetSecurityString(elevatedSecurity_ ?
-                pipe::SecurityMode::Service : pipe::SecurityMode::Child);
-        }
-        // data
-        std::string pipeName_;
-        bool elevatedSecurity_; // for now, assume that security is elevated only when using default pipe name
-        as::io_context ioctx_;
-        act::AsyncActionManager<ServiceExecutionContext> actionManager_;
-        as::windows::object_handle stopEvent_;
-    };
 
     ActionServer::ActionServer(Service* pSvc, PresentMon* pPmon, std::optional<std::string> pipeName)
     {
-        std::thread([=, pipeName = std::move(pipeName)] {
-            ActionServerImpl_ impl(pSvc, pPmon, std::move(pipeName));
-            impl.Run_();
-        }).detach();
+        // if we have a pipe name override
+        auto sec = pipe::DuplexPipe::GetSecurityString(!pipeName ?
+            pipe::SecurityMode::Service : pipe::SecurityMode::Child);
+        act::ActionServerImpl_<ServiceExecutionContext>::LaunchThread(
+            ServiceExecutionContext{ .pSvc = pSvc, .pPmon = pPmon },
+            pipeName.value_or(gid::defaultControlPipeName), 2, std::move(sec)
+        ).detach();
     }
 }
