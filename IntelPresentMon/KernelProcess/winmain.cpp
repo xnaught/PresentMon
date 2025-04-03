378ab6f5841dffd10c0fe635990ea306bd9dcde4
Chili <planetchili@users.noreply.github.com>
2025-04-03 11:15:20 +0900

    kernel process winmain setup

---
+++ IntelPresentMon/KernelProcess/winmain.cpp
@@ -0,0 +1,8 @@
+#include "../CommonUtilities/win/WinAPI.h"
+#include <boost/process.hpp>
+
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	return 0;
+}
