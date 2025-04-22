9b66a58a9e19635aabcba0ca91722cf4b12466a7
Chili <planetchili@users.noreply.github.com>
2025-04-22 11:35:35 +0900

    minimal impl of the hotkey store

---
+++ IntelPresentMon/AppCef/ipm-ui-vue/src/stores/hotkey.ts
@@ -0,0 +1,89 @@
+import { ref, reactive, readonly } from 'vue'
+import { defineStore } from 'pinia'
+import type { ModifierOption, KeyOption, Binding, KeyCode, ModifierCode } from '@/core/hotkey'
+import { Action as HotkeyAction } from '@/core/hotkey'
+import { Api } from '@/core/api'
+import { getEnumValues } from '@/core/meta'
+
+export const useHotkeyStore = defineStore('hotkey', () => {
+    // === State ===
+    const keyOptions = ref<KeyOption[]>([])
+    const modifierOptions = ref<ModifierOption[]>([])
+    const bindings = reactive<{ [key: string]: Binding }>(
+        // add an entry for each action in the (Hotkey)Action enum
+        Object.fromEntries(
+            getEnumValues(HotkeyAction).map((action) => [
+                HotkeyAction[action],
+                { action, combination: null },
+            ])
+        )
+    )
+    // TODO: receive this from kernel or generate based on SSoT
+    const defaultBindings = readonly<Binding[]>([
+        {
+          action: HotkeyAction.ToggleCapture,
+          combination: { key: 42 as KeyCode, modifiers: [2 as ModifierCode, 4 as ModifierCode] },
+        },
+        {
+          action: HotkeyAction.CyclePreset,
+          combination: { key: 47 as KeyCode, modifiers: [2 as ModifierCode, 4 as ModifierCode] },
+        },
+        {
+          action: HotkeyAction.ToggleOverlay,
+          combination: { key: 46 as KeyCode, modifiers: [2 as ModifierCode, 4 as ModifierCode] },
+        },
+    ]) as readonly Binding[]
+
+    // === Actions ===
+    // use the api to get the keys/modifiers from the kernel
+    async function refreshOptions() {
+        keyOptions.value = await Api.enumerateKeys()
+        modifierOptions.value = await Api.enumerateModifiers()
+    }
+    // bind default combinations to the actions
+    async function bindDefaults() {
+      for (const binding of defaultBindings) {
+        try {
+          await bindHotkey(binding)
+        } catch (e) {
+          // TODO Notifications.notify({ text: `Unable to bind default hotkey for ${HotkeyAction[binding.action]}` })
+        }
+      }
+    }
+    // set a hotkey combination for an action
+    async function bindHotkey(binding: Binding) {
+      try {
+        await Api.bindHotkey(binding)
+        bindings[HotkeyAction[binding.action]] = binding
+      } catch (e) {
+        const actionName = HotkeyAction[binding.action]
+        // TODO Notifications.notify({ text: `Failed to bind hotkey for [${actionName}]` })
+        console.error([`Failed to bind hotkey; Action: [${actionName}]`, e])
+      }
+    }
+    // clear the hotkey combination for an action (make it unbound)
+    async function clearHotkey(action: HotkeyAction) {
+      try {
+        await Api.clearHotkey(action)
+        bindings[HotkeyAction[action]] = { action, combination: null }
+      } catch (e) {
+        const actionName = HotkeyAction[action]
+        // TODO Notifications.notify({ text: `Failed to clear hotkey for [${actionName}]` })
+        console.error([`Failed to clear hotkey; Action: [${actionName}]`, e])
+      }
+    }
+
+    // === Exports ===
+    return {
+        // === State ===
+        keyOptions,
+        modifierOptions,
+        bindings,
+        defaultBindings,      
+        // === Actions ===
+        refreshOptions,
+        bindDefaults,
+        bindHotkey,
+        clearHotkey,
+      }
+})
