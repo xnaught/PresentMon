5272024109196a3f366884a3435e196a82fc6180
Chili <planetchili@users.noreply.github.com>
2025-06-13 06:34:04 +0900

    Revert "hardcoding out etl branches"
    This reverts commit 08f81cc48136928bd62c406e6ac1e26dece3588e.

--- IntelPresentMon/Streamer/StreamClient.cpp
+++ IntelPresentMon/Streamer/StreamClient.cpp
@@ -183,7 +183,7 @@ void StreamClient::PeekPreviousFrames(const PmNsmFrameData** pFrameDataOfLastPre
         uint32_t numFramesTraversed = 0;
         while (pTempFrameData)
         {
-            if (false) {
+            if (nsm_hdr->from_etl_file) {
                 peekIndex = (peekIndex == 0) ? current_max_entries : peekIndex - 1;
                 if (peekIndex == nsm_hdr->tail_idx) {
                     return;
@@ -315,7 +315,7 @@ PM_STATUS StreamClient::ConsumePtrToNextNsmFrameData(const PmNsmFrameData** pNsm
         // dequeue frame number. This will be used to track data overruns if
         // the client does not read data fast enough.
         recording_frame_data_ = true;
-        if (false) {
+        if (nsm_hdr->from_etl_file) {
             current_dequeue_frame_num_ = nsm_hdr->head_idx;
             next_dequeue_idx_ = nsm_hdr->head_idx;
         }
@@ -385,7 +385,7 @@ PM_STATUS StreamClient::ConsumePtrToNextNsmFrameData(const PmNsmFrameData** pNsm
             pFrameDataOfPreviousAppFrameOfLastAppDisplayed);
 
         current_dequeue_frame_num_++;
-        if (false) {
+        if (nsm_hdr->from_etl_file) {
             nsm_view->DequeueFrameData();
         }
         return PM_STATUS::PM_STATUS_SUCCESS;
